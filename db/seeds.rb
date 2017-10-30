dcbaba06e555a9bde6e8091a502a97e96dc7efdc
“rjarin56” <reniejarin@gmail.com>
2017-10-29 20:37:41 -0500

    Added Products model, Added faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  Product.create(name: Faker::Name.name, description: Faker::Name.title)
+end
